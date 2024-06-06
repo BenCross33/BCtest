d1607c3afe2bdcfc5cb6b10b0cfd0157e64f3494
BenCross33 <ben.cross@communities.gov.uk>
2024-06-06 15:03:03 +0100

    updates

--- R/create_markdown.R
+++ R/create_markdown.R
@@ -2,30 +2,33 @@
 #' @param input_path A filepath of an excel table to import.
 #' @param output_path A filepath of an excel table to export
 #' @returns Rmarkdown file.
+#' @export
 create_markdown <- function(input_path, output_path){
-
+#import data
   df <- import_data(input_path)
 
-
+#make data changes
   df[2, 2:ncol(df)] <- 50
+#export data
+  writexl::write_xlsx(df, output_path)
 
-  write_xlsx(df, output_path)
 
+#get needed values
   march_c_value <- as.numeric(df[df$Variables == "C", "March"])
   april_c_value <- as.numeric(df[df$Variables == "C", "April"])
 
-
+#list parameters
   params <- list(
     parameter1 = march_c_value,
     parameter2 = april_c_value
   )
 
+#find rmd file and craete rmd export location
   rmdFilename <- system.file("rmd", "testing.rmd", package = "BCtest")
-
   word_filename <- gsub(".Rmd$", ".doc", rmdFilename)
 
-  rmarkdown::render(input =rmdFilename, output_file = word_filename, params = params, envir =  new.env())
-
+#generate and open word created from markdown
+  rmarkdown::render(input =rmdFilename, output_file = word_filename, params = params, envir =  new.env(), overwrite = TRUE)
   browseURL(word_filename)
 
 
--- R/import_data.R
+++ R/import_data.R
@@ -5,9 +5,10 @@
 import_data <- function(input_path){
 
   # Read the Excel file and save it as df1
-  df1 <- read_excel(input_path)
+  df1 <- readxl::read_excel(input_path)
 
   # Return the data frame
   return(df1)
 
 }
+
